cec8665306b07a41480bffee50c60b5c1ff2784f
Ross Lovas <rosslovas@gmail.com>
2019-07-11 22:29:15 +0930

    can draw shapes on the canvas

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,62 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+let drawing = false;
+let startX: number;
+let startY: number;
+let currentPath: Path2D;
+let newPath: [number, number][];
+const paths: [number, number][][] = [];
+
 const App: React.FC = () => {
+	const canvasRef = React.useRef<HTMLCanvasElement>(null);
+	function getCtx() {
+		return canvasRef.current!.getContext('2d')!;
+	}
 	return (
 		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.tsx</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
+			<canvas
+				ref={canvasRef}
+				width={window.innerWidth}
+				height={window.innerHeight}
+				onMouseDown={e => {
+					const ctx = getCtx();
+					ctx.strokeStyle = '#F00';
+					ctx.fillStyle = '#0F0';
+					ctx.lineWidth = 3;
+					ctx.beginPath();
+					ctx.moveTo(e.clientX, e.clientY);
+					currentPath = new Path2D();
+					currentPath.moveTo(e.clientX, e.clientY);
+					newPath = [];
+					newPath.push([e.clientX, e.clientY]);
+					startX = e.clientX;
+					startY = e.clientY;
+					drawing = true;
+				}}
+				onMouseUp={e => {
+					const ctx = getCtx();
+					if (drawing) {
+						newPath.push([startX, startY]);
+						paths.push(newPath);
+						console.log(paths);
+						ctx.lineTo(startX, startY);
+						currentPath.lineTo(startX, startY);
+						ctx.stroke();
+						ctx.fill();
+						drawing = false;
+					}
+				}}
+				onMouseMove={e => {
+					const ctx = getCtx();
+					if (drawing) {
+						ctx.lineTo(e.clientX, e.clientY);
+						currentPath.lineTo(e.clientX, e.clientY);
+						newPath.push([e.clientX, e.clientY]);
+						ctx.stroke();
+					}
+				}}
+			/>
 		</div>
 	);
 }
